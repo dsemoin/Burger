4058526b5b6deb086612bbe8f710970ceb3ac86b
Darlene Holland <darlene@poseidon.lan>
2018-05-06 19:19:29 -0400

    devour, layout, better burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -40,7 +40,7 @@ router.post("/api/burgers", function (req, res) {
   }
 });
 
-router.put("/api/burgers/:id", function (req, res) {
+router.post("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -52,7 +52,7 @@ router.put("/api/burgers/:id", function (req, res) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
-      res.status(200).end();
+      res.redirect('/');
     }
   });
 });
