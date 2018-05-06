6aa3f88437beba39fdecd6b68692658c74f248aa
Darlene Holland <darlene@poseidon.lan>
2018-05-06 19:35:15 -0400

    Support for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 // Set up mysql connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 // Make connections
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.log("error connecting: " + err.stack);
         return;
